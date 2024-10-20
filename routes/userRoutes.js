d99914e6ab2db8e5347de2b89b836ae015f15b46
Daigy Sarma <daisysarma20@gmail.com>
2024-10-20 21:33:11 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,10 @@ import userLogin from "../controllers/userLogin.js";
 import loggedUser from "../controllers/loggedUser.js";
 import cors from "cors";
 
-
 router.post('/log-out', cors(), loggedUser);
 
 router.post('/reg', cors(), userReg);
 router.post('/login', cors(), userLogin);
-
 
 export default router;
 
